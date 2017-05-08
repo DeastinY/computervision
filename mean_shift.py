1e387625143f6ff11d775ae5c3f890310505fe47
DeastinY <polzin.richard@gmail.com>
2017-05-09 00:34:29 +0200

    switche to cKDTree for incredible performance gains

--- mean_shift.py
+++ mean_shift.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # # Mean Shift
@@ -7,26 +6,27 @@
 
 # In[332]:
 
-import sys
-import cv2
 import math
-import matplotlib.pyplot as plt
-import matplotlib.pylab as pylab
-from mpl_toolkits.mplot3d import Axes3D
+import sys
 import time
-import requests
-import numpy as np
 from pathlib import Path
-from scipy.io import loadmat
-from scipy import spatial
-from sklearn.datasets import *
+
+import cv2
+import matplotlib.pylab as pylab
+import matplotlib.pyplot as plt
+import numpy as np
+import requests
 from IPython.core.display import clear_output
-from util import log_progress
+from mpl_toolkits.mplot3d import Axes3D
 from progress.bar import Bar
+from scipy import spatial
+from scipy.io import loadmat
+from sklearn.datasets import *
+from util import log_progress
+
 # get_ipython().magic('matplotlib inline')
 pylab.rcParams['figure.figsize'] = 16, 12
 sys.setrecursionlimit(10000)
-
 
 # ##### Load  and visualize sample data
 # The matrix is loaded into a numpy array of dimensions (2000, 3)
@@ -44,7 +44,6 @@ IMG_A = cv2.imread(str(img_path / "a.jpg"))
 IMG_B = cv2.imread(str(img_path / "b.jpg"))
 IMG_C = cv2.imread(str(img_path / "c.jpg"))
 
-
 # ##### Utility functions
 # 
 
@@ -52,10 +51,12 @@ IMG_C = cv2.imread(str(img_path / "c.jpg"))
 
 cached_tree = None
 
+
 def get_neighbours(data, point, r):
     global cached_tree
-    cached_tree = spatial.KDTree(data) if cached_tree is None else cached_tree
+    cached_tree = spatial.cKDTree(data) if cached_tree is None else cached_tree
     return cached_tree.query_ball_point(point, r)
+
 
 def get_neighbours_cdist(data, point, r):
     distances = spatial.distance.cdist(np.array([point]), data)[0]
@@ -64,10 +65,10 @@ def get_neighbours_cdist(data, point, r):
 
 # In[335]:
 
-def find_peak(data, point, r, t = 0.01):
+def find_peak(data, point, r, t=0.01):
     def calc_new_shift(data, point, r):
         return data[get_neighbours(data, point, r)].mean(axis=0)
-    
+
     dist = t
     while dist >= t:
         peak = calc_new_shift(data, point, r)
@@ -75,18 +76,19 @@ def find_peak(data, point, r, t = 0.01):
         point = peak
     return peak
 
+
 def meanshift(data, r):
     peaks, points, point_peaks = [], [], []
     for point in log_progress(data, 1, len(data)):
         peak = find_peak(data, point, r)
         # Match peak to possible neighbours. Use cdist because we have only few peaks
-        neighbours = get_neighbours_cdist(np.array(peaks), peak, r/2.) if len(peaks) > 0 else []
+        neighbours = get_neighbours_cdist(np.array(peaks), peak, r / 2.) if len(peaks) > 0 else []
         if len(neighbours) > 1:
             peak = neighbours[0]
         else:
             peaks.append(peak)
         points.append(point)
-        point_peaks.append(np.where(peaks==peak)[0][0])
+        point_peaks.append(np.where(peaks == peak)[0][0])
     return np.array(peaks), np.array(points), np.array(point_peaks)
 
 
@@ -95,18 +97,19 @@ def meanshift(data, r):
 def find_peak_opt(data, point, r, c=4.0, t=0.01):
     def calc_new_shift(data, point, r):
         return data[get_neighbours(data, point, r)].mean(axis=0)
-    
+
     dist = t
     cpts = set()
     while dist >= t:
         peak = calc_new_shift(data, point, r)
         dist = spatial.distance.euclidean(peak, point)
-        cpts = cpts.union(get_neighbours(data, point, r/c))
+        cpts = cpts.union(get_neighbours(data, point, r / c))
         point = peak
     return peak, list(cpts)
 
+
 def meanshift_opt(data, r):
-    peaks, point_peaks = [], np.zeros(data.shape[0], dtype='int16')-1
+    peaks, point_peaks = [], np.zeros(data.shape[0], dtype='int16') - 1
     bar = Bar("Progress:", max=len(data))
     for i, point in enumerate(data):
         bar.next()
@@ -114,7 +117,7 @@ def meanshift_opt(data, r):
             continue
         peak, cpts = find_peak_opt(data, point, r)
         # Match peak to possible neighbours. Use cdist because we have only few peaks
-        peak_neighbours = get_neighbours_cdist(np.array(peaks), peak, r/2.) if len(peaks) > 0 else []
+        peak_neighbours = get_neighbours_cdist(np.array(peaks), peak, r / 2.) if len(peaks) > 0 else []
         if len(peak_neighbours) > 1:
             peak = data[neighbours][0]
         else:
@@ -132,11 +135,11 @@ def meanshift_opt(data, r):
 
 def image_segment(image, r, scale=0.5):
     # preprocess the image
-    image = cv2.resize(image, None, fx = scale, fy = scale)
+    image = cv2.resize(image, None, fx=scale, fy=scale)
     orig_image = np.array(image)
-    image = cv2.GaussianBlur(image, (5,5), 5.0)
+    image = cv2.GaussianBlur(image, (5, 5), 5.0)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
-    image = image.reshape(image.shape[0]*image.shape[1], image.shape[2])
+    image = image.reshape(image.shape[0] * image.shape[1], image.shape[2])
     print("Image has {} points".format(image.shape))
     peaks, point_peaks = meanshift_opt(image, r)
     print("Found {} peaks !".format(len(peaks)))
@@ -164,6 +167,5 @@ def visualize(image, r, func):
 
 # In[ ]:
 
-#visualize(SAMPLE_DATA, r=5, func=meanshift_opt)
+# visualize(SAMPLE_DATA, r=5, func=meanshift_opt)
 image_segment(IMG_A, r=6)
-
