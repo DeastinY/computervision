47fcddba4d1305329800aede39e32b6fb7cc72c6
DeastinY <polzin.richard@gmail.com>
2017-05-28 13:59:37 +0200

    Refactored and added flag --pc

--- eigenfaces/eigenfaces.py
+++ eigenfaces/eigenfaces.py
@@ -24,37 +24,43 @@ def pca(data, k):
     return mean, eigenpairs[:k]
 
 
-def eigen_analysis(data, labels, k, plot=True):
+def plot_eigenface(value, vector):
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.imshow(vector.T, cmap="Greys")
+    plt.title(f"Eigenvalue: {value}")
+    plt.waitforbuttonpress()
+
+
+def eigen_analysis(data, labels, k, plot):
     mean, eigenpairs = pca(data, k)
-    for value, vector in eigenpairs:
-        vector = vector.reshape((32, 32))
-        if plot:
-            fig = plt.figure()
-            ax = fig.add_subplot(1, 1, 1)
-            ax.imshow(vector.T, cmap="Greys")
-            plt.title(f"Eigenvalue: {value}")
-            plt.waitforbuttonpress()
+    [plot_eigenface(value, vector.reshape((32, 32))) for value, vector in eigenpairs if plot]
+
+
+def process_data(data, train):
+    labels, data = np.array(data["gnd"]), np.array(data["fea"])
+    data = data / 255.  # normalize
+    # All credits to Christoph Emunds (@Fallscout) for figuring that out:
+    # Need to subtract 1 from the indices, because matrices
+    # have originally been created in MATLAB and
+    # MATLAB indexing starts at 1 as opposed to 0
+    dtrain = data[raw_train["trainIdx"].flatten() - 1]
+    ltrain = labels[raw_train["trainIdx"].flatten() - 1].flatten()
+    dtest = data[raw_train["testIdx"].flatten() - 1]
+    lttest = labels[raw_train["testIdx"].flatten() - 1].flatten()
+    return dtrain, labels
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # The train file contains the images for
     parser.add_argument("-data", default="images/ORL_32x32.mat", help="The file with all the image data.")
-    parser.add_argument("-train", default="images/3Train/3.mat", help="The file with training indices. 3Train, 5Train or 7Train.")
+    parser.add_argument("-train", default="images/3Train/3.mat", help="The file with training indices. (3/5/7Train)")
     parser.add_argument("-k", default=20, help="How many principal components to use.")
+    parser.add_argument("--pc", action='store_true', help="Set flag to plot the first k principal components")
     args = vars(parser.parse_args())
 
     raw_data, raw_train = loadmat(args["data"]), loadmat(args["train"])
-    nlabels, ndata = np.array(raw_data["gnd"]), np.array(raw_data["fea"])
-    ndata = ndata / 255.  # normalize
+    train_data, nlabels = process_data(raw_data, raw_train)
 
-    # All credits to Christoph Emunds (@Fallscout) for figuring that out:
-    # Need to subtract 1 from the indices, because matrices
-    # have originally been created in MATLAB and
-    # MATLAB indexing starts at 1 as opposed to 0
-    train_data = ndata[raw_train["trainIdx"].flatten() - 1]
-    train_labels = nlabels[raw_train["trainIdx"].flatten() - 1].flatten()
-    test_data = ndata[raw_train["testIdx"].flatten() - 1]
-    test_labels = nlabels[raw_train["testIdx"].flatten() - 1].flatten()
-
-    eigen_analysis(train_data, nlabels, args["k"])
+    eigen_analysis(train_data, nlabels, args["k"], args["pc"])
